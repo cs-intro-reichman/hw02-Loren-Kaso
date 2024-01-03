25429fde2af7968bff98ce37b6808ef5c9af5da9
loren.kaso <loren.kaso@post.runi.ac.il>
2024-01-03 17:09:26 +0200

    test1

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
-		// Scanner s1 = new Scanner(System.in);
-		// int num = s1.nextInt();
-		// for (int i = 0; i < num; i++) {
-		// 	for (int j = 0; j < num; j++) {
-		// 		if(i % 2 == 0 ){
-		// 			System.out.print("* " );
-		// 		}else{
-		// 			System.out.print(" *" );
-		// 		}
-		// 	}
-		// 	System.out.println();
+		Scanner s1 = new Scanner(System.in);
+		int num = s1.nextInt();
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num; j++) {
+				if(i % 2 == 0 ){
+					System.out.print("* " );
+				}else{
+					System.out.print(" *" );
+				}
+			}
+			System.out.println();
 		}
 	}
-//}
+}
